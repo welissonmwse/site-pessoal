ee4a6608536765bd231eb0e21a435c1ed63615e3
Welisson Silveira <welissonmwse@gmail.com>
2021-10-17 11:15:29 -0300

    adiciona responsividade menu

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ window.addEventListener('scroll', () => {
     const header = document.querySelector('.container header')
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.container header nav ul').classList.toggle('active')
+})
