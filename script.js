f8e55ffaca256a75d4178e3eb06e2ceff1241ab4
Welisson Silveira <welissonmwse@gmail.com>
2021-11-04 18:58:33 -0300

    adiciona funcionalidade scroll smoll

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ window.addEventListener('scroll', () => {
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.container header nav ul').classList.toggle('active')
 })
+
+const links = document.querySelectorAll("header nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
