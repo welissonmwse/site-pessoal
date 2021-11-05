6dbc80986135ef79a1d195c93878028e57f9b2b7
Welisson Silveira <welissonmwse@gmail.com>
2021-11-05 19:45:39 -0300

    adiciona animacao ao menu mobile

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('.container header')
+    const header = document.querySelector('#header')
     header.classList.toggle('sticky', window.scrollY > 0)
-})
-
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.container header nav ul').classList.toggle('active')
 })
 
 const links = document.querySelectorAll("header nav ul li a");
@@ -23,3 +19,21 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
